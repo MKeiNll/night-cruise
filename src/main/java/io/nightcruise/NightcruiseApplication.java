3b0ba2056e490f89807fa694aea3832333430c7c
MKeiNll <levgirich@gmail.com>
2019-10-10 17:26:04 +0300

    initial commit

---
+++ src/main/java/io/nightcruise/NightcruiseApplication.java
@@ -0,0 +1,13 @@
+package io.nightcruise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NightcruiseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NightcruiseApplication.class, args);
+	}
+
+}
