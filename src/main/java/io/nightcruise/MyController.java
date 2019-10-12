7ae585b64fd8e27403eab246cca1813b467ca8a7
MKeiNll <levgirich@gmail.com>
2019-10-12 16:32:57 +0300

    initial theme design

--- src/main/java/io/nightcruise/MyController.java
+++ src/main/java/io/nightcruise/MyController.java
@@ -4,35 +4,43 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-
 
 @Controller
 public class MyController {
 
+    public static final String HOME_PAGE_TITLE = "Night Cruise";
+    public static final String ABOUT_PAGE_TITLE = "About";
+    public static final String DEV_PAGE_TITLE = "Developments";
+    public static final String TAR_PAGE_TITLE = "Tar";
+
+    public static final String ABOUT_MENU_LABEL = "About";
+    public static final String DEV_MENU_LABEL = "Developments";
+    public static final String TAR_MENU_LABEL = "Tar";
+
+    public static final String HOME_SIDE_MENU_LABEL = "Home";
+    public static final String THEME_SIDE_MENU_LABEL = "Theme";
+
     @GetMapping("/")
     public String getHome(Model model) {
-        model.addAttribute("menu", Arrays.asList("About", "Developments", "Tar"));
+        model.addAttribute("menu", new String[][]{{HOME_PAGE_TITLE}, {ABOUT_MENU_LABEL, DEV_MENU_LABEL, TAR_MENU_LABEL}, {THEME_SIDE_MENU_LABEL}});
         return "home.html";
     }
 
     @GetMapping("/about")
     public String getAbout(Model model) {
-        model.addAttribute("menu", Arrays.asList("Developments", "Tar"));
+        model.addAttribute("menu", new String[][]{{ABOUT_PAGE_TITLE}, {DEV_MENU_LABEL, TAR_MENU_LABEL}, {HOME_SIDE_MENU_LABEL}});
         return "about.html";
     }
 
     @GetMapping("/developments")
     public String getDevelopments(Model model) {
-        model.addAttribute("menu", Arrays.asList("About", "Tar"));
+        model.addAttribute("menu", new String[][]{{DEV_PAGE_TITLE}, {ABOUT_MENU_LABEL, TAR_MENU_LABEL}, {HOME_SIDE_MENU_LABEL}});
         return "developments.html";
     }
 
     @GetMapping("/tar")
     public String getTar(Model model) {
-        model.addAttribute("menu", Arrays.asList("About", "Developments"));
+        model.addAttribute("menu", new String[][]{{TAR_PAGE_TITLE}, {ABOUT_MENU_LABEL, DEV_MENU_LABEL}, {HOME_SIDE_MENU_LABEL}});
         return "tar.html";
     }
 }
