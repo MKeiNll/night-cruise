6b3542671b1ee8ffc655934665dacbe038cfc1fb
MKeiNll <levgirich@gmail.com>
2019-10-25 22:21:30 +0300

    implement time passed

--- src/main/java/io/nightcruise/MyController.java
+++ src/main/java/io/nightcruise/MyController.java
@@ -35,8 +35,8 @@ public class MyController {
     public String getAbout(Model model) {
         model.addAttribute("menu", new String[][]{{ABOUT_PAGE_TITLE}, MENU_LABELS, {HOME_MENU_LABEL}});
         model.addAttribute("current", "about");
-        model.addAttribute("aboutCreated", "12.12.2008");
-        model.addAttribute("aboutUpdated", "21.03.2014");
+        model.addAttribute("aboutCreated", "25.10.2019");
+        model.addAttribute("aboutUpdated", "25.10.2019");
         model.addAttribute("cvUpdated", "21.08.2015");
         return "about";
     }
