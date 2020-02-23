49dde3cd7b78589e03d0d0c14d32824ea66e427a
MKeiNll <levgirich@gmail.com>
2020-02-23 10:33:22 +0200

    remove hardcoded text from controller

--- src/main/java/io/nightcruise/MyController.java
+++ src/main/java/io/nightcruise/MyController.java
@@ -9,26 +9,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MyController {
-
-    public static final String HOME_PAGE_TITLE = "Night Cruise";
-    public static final String ABOUT_PAGE_TITLE = "About";
-    public static final String DEV_PAGE_TITLE = "Developments";
-    public static final String SUPER_3_PAGE_TITLE = "SUPER_3";
-    public static final String TAR_PAGE_TITLE = "TAR";
-
-    public static final String ABOUT_MENU_LABEL = "About";
-    public static final String DEV_MENU_LABEL = "Developments";
-    public static final String TAR_MENU_LABEL = "TAR";
-    public static final String SUPER_3_MENU_LABEL = "SUPER_3";
-    public static final String HOME_MENU_LABEL = "Home";
-    public static final String THEME_MENU_LABEL = "Theme";
-    public static final String LANGUAGE_MENU_LABEL = "Language";
-
-    public static final String[] MENU_LABELS = {DEV_MENU_LABEL, ABOUT_MENU_LABEL, THEME_MENU_LABEL, LANGUAGE_MENU_LABEL};
-
     @RequestMapping(value = {"/"}, method = {RequestMethod.GET, RequestMethod.POST})
     public String getHome(Model model) {
-        model.addAttribute("menu", new String[][]{{HOME_PAGE_TITLE}, MENU_LABELS});
         model.addAttribute("current", "home");
         return "home";
     }
