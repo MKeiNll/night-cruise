b4bf66b8a0196f4bad60fb408652e641559a187e
MKeiNll <levgirich@gmail.com>
2020-03-08 21:22:43 +0200

    fix theme icons

--- src/main/java/io/nightcruise/MyController.java
+++ src/main/java/io/nightcruise/MyController.java
@@ -8,19 +8,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MyController {
-    public static final String LIGHT_THEME_LOGO = "MKeiN2";
-    public static final String VIVID_THEME_LOGO = "Saline";
-
     @RequestMapping(value = {"/"}, method = {RequestMethod.GET, RequestMethod.POST})
     public String getHome(Model model) {
-        model.addAttribute("current", "home");
-        model.addAttribute("theme", LIGHT_THEME_LOGO);
+        model.addAttribute("currentPage", "home");
+        model.addAttribute("currentTheme", "light");
         return "home";
     }
 
     @GetMapping("/about")
     public String getAbout(Model model) {
-        model.addAttribute("current", "about");
+        model.addAttribute("currentPage", "about");
+        model.addAttribute("currentTheme", "light");
         model.addAttribute("websiteCreated", "23.02.2020");
         model.addAttribute("websiteUpdated", "28.02.2020");
         return "about";
@@ -28,23 +26,12 @@ public class MyController {
 
     @GetMapping("/developments")
     public String getDevelopments(Model model) {
-        model.addAttribute("current", "developments");
+        model.addAttribute("currentPage", "developments");
+        model.addAttribute("currentTheme", "light");
         model.addAttribute("sporexUpdated", "28.11.2018");
         model.addAttribute("sporexCreated", "21.06.2018");
         model.addAttribute("mbfCreated", "21.11.2016");
         model.addAttribute("mbfUpdated", "05.01.2017");
         return "developments";
-    }
-
-    @GetMapping("/tar")
-    public String getTar(Model model) {
-        model.addAttribute("current", "tar");
-        return "tar";
-    }
-
-    @GetMapping("/super_3")
-    public String getSuper3(Model model) {
-        model.addAttribute("current", "super_3");
-        return "super_3";
     }
 }
