ab4f1c8b2a0fc888c2c5da10fd8a9a4abada2097
MKeiNll <levgirich@gmail.com>
2020-03-11 21:58:06 +0200

    theme switch functionality

--- src/main/java/io/nightcruise/ErrorController.java
+++ src/main/java/io/nightcruise/ErrorController.java
@@ -1,12 +1,11 @@
 package io.nightcruise;
 
-import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class MyErrorController implements ErrorController {
+public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
 
     @RequestMapping("/error")
     public String handleError() {
---
+++ src/main/java/io/nightcruise/TemplateController.java
@@ -0,0 +1,77 @@
+package io.nightcruise;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+class SetThemeRequestBody {
+    public String theme;
+}
+
+@Controller
+public class MyController { // TODO - set data attarasd to html
+
+    public static final int THEME_COOKIE_MAX_AGE = 10 * 365 * 24 * 60 * 60; // 10 years
+
+    public static final String DEFAULT_THEME = "light";
+
+    @RequestMapping(value = {"/"}, method = {RequestMethod.GET, RequestMethod.POST})
+    public String getHome(@CookieValue(required = false, value = "theme") String themeCookie, Model model) {
+        setTheme(themeCookie, model);
+        model.addAttribute("currentPage", "home");
+        return "home";
+    }
+
+    @RequestMapping(value = {"/setTheme"}, method = {RequestMethod.POST})
+    @ResponseBody
+    public ResponseEntity setTheme(@RequestBody(required = false) SetThemeRequestBody setThemeRequest,
+                                   HttpServletResponse response) throws IOException {
+        Cookie themeCookie = new Cookie("theme", setThemeRequest.theme);
+        themeCookie.setMaxAge(THEME_COOKIE_MAX_AGE);
+        response.addCookie(themeCookie);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    public void setTheme(String themeCookie, Model model) {
+        boolean isRandomTheme = false;
+        if (themeCookie != null) {
+            if (themeCookie.equals("random")) {
+                model.addAttribute("currentTheme", DEFAULT_THEME); // TODO - random theme functionality
+                isRandomTheme = true;
+            } else {
+                model.addAttribute("currentTheme", themeCookie);
+            }
+        } else {
+            model.addAttribute("currentTheme", DEFAULT_THEME);
+        }
+        model.addAttribute("isRandomTheme", isRandomTheme);
+    }
+
+    @GetMapping("/about")
+    public String getAbout(@CookieValue(value = "theme", required = false) String themeCookie, Model model) {
+        setTheme(themeCookie, model);
+        model.addAttribute("currentPage", "about");
+
+        model.addAttribute("websiteCreated", "23.02.2020");
+        model.addAttribute("websiteUpdated", "28.02.2020");
+        return "about";
+    }
+
+    @GetMapping("/developments")
+    public String getDevelopments(@CookieValue(value = "theme", required = false) String themeCookie, Model model) {
+        setTheme(themeCookie, model);
+        model.addAttribute("currentPage", "developments");
+
+        model.addAttribute("sporexUpdated", "28.11.2018");
+        model.addAttribute("sporexCreated", "21.06.2018");
+        model.addAttribute("mbfCreated", "21.11.2016");
+        model.addAttribute("mbfUpdated", "05.01.2017");
+        return "developments";
+    }
+}
